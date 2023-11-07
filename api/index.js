c170f0c3a841b6ff10ac4ea8ff5f431ddbe46cc6
Saqib Hussain <dev.saqibhussain@gmail.com>
2023-11-07 09:20:39 +0500

    test

--- api/index.js
+++ api/index.js
@@ -1,18 +1,4 @@
 import app from "../index.js";
-import mongoose from "mongoose";
-import * as dotevn from "dotenv";
-dotevn.config();
 
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log("Connected to database");
-    app.listen(process.env.PORT || 5555, () => {
-      console.log(`App is listening on PORT: ${process.env.PORT || 5555}`);
-    });
-  })
-  .catch((err) => {
-    console.log("Error message:", err);
-  });
 
 export default app;
